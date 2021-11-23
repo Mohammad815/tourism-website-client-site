bf3a67bf7f59ee5129ba77ea982925eb4d2633ff
Mohammad815 <mohammadforhad176@gmail.com>
2021-11-23 18:33:14 +0600

    update

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -13,7 +13,7 @@ const Banner = () => {
              <Row className="banner mt-3">
                 <div className="col-md-6 col-12">
                     <h1 className="title">Make Your Tour Amazing With Us</h1>
-                    <h5 className="blockquote">Air seed winged lights saw kind whales in sixth dont seas dron image so fish all tree meat dont there is seed winged lights saw kind whales in sixth dont seas dron image so fish all tree meat dont there</h5>
+                    <h5 className="blockquote">Air seed winged lights saw kind whales in sixth dont seas dron image so fish all tree meat dont there is seed winged lights saw kind whales in sixth dont seas dron image so fish all tree meat dont there location</h5>
                     <Link to="/home"><Button className="btn-common rounded-pill mt-2" size="lg">
                          Get Started
                     </Button></Link>
